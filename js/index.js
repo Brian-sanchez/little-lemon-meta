a517e073c8565a2dd4f5e1e6b0e9a546c3728ce0
Brian Sanchez <briansanchezc17@gmail.com>
2024-03-25 18:45:51 -0300

    Primera version

---
+++ js/index.js
@@ -0,0 +1,4 @@
+// Lemon Copyright
+const actualDate = new Date();
+const element = document.getElementById("lemon-copyright");
+element.innerHTML = `Copyright © ${actualDate.getFullYear()} Little Lemon`;
